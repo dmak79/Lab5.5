e9f592fc3e2a6b5ef30b23cf2553d16aa22c5ce8
Home <Home@DESKTOP-GOHQSKE>
2021-11-21 18:39:39 +0300

    Добавьте файлы проекта.

---
+++ Lab5.5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк и столбцов массива ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int[,] array = new int[N, N];
+
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    array[i, j] = 1 - (i + j) % 2;
+                    Console.Write(array[i, j] + " ");
+                }
+
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }
+    }
+}
